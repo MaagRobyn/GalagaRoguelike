b0fd01ebebdd1434dfdefab2591fcd78f2895dcb
Robyn Maag <maag.matthew@outlook.com>
2024-01-16 22:21:40 -0800

    Manually merge into main

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,9 +1,11 @@
+using Assets;
 using Assets.Scripts;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -15,11 +17,10 @@ public class GameManager : MonoBehaviour
     private List<ShipScript> existingShips = new();
     private EncounterType encounterType;
 
-    //[SerializeField] Dictionary<ProjectileType, GameObject> projectiles = new Dictionary<ProjectileType, GameObject>();
-    //[SerializeField] Dictionary<AlienType, GameObject> aliens = new Dictionary<AlienType, GameObject>();
+    [SerializeField] Button optionButton;
     [SerializeField] GameObject basicProjectile;
     [SerializeField] AlienShipScript basicAlien;
-    [SerializeField] Canvas canvas;
+    [SerializeField] RectTransform rewardMenu;
     [SerializeField] RadarScript radar;
     [SerializeField] Transform radarHolder;
     [SerializeField] TextMeshProUGUI bountyText;
@@ -32,7 +33,7 @@ public class GameManager : MonoBehaviour
     {
         Instance = this;
         AddBounty(5.0f);
-        encounterType = EncounterType.Endless;
+        encounterType = EncounterType.Basic;
     }
 
     // Update is called once per frame
@@ -43,6 +44,7 @@ public class GameManager : MonoBehaviour
             Debug.Log("Quitting Game");
             Application.Quit();
         }
+        
         if(spawnDelay > 0)
         {
             spawnDelay -= Time.deltaTime;
@@ -77,19 +79,66 @@ public class GameManager : MonoBehaviour
                     currentDangerLevel = 0;
                     AddBounty(1.0f);
                     break;
+                case EncounterType.Basic:
+                    spawnDelay = 0;
+                    roundHasEnded = false;
+                    roundCanEnd = false;
+                    currentDangerLevel = 0;
+                    AddBounty(1.0f);
+                    OpenMenu(3);
+                    break;
                 default:
                     break;
             }
         }
         
     }
-
+    private void OpenMenu(int rewardCount)
+    {
+        List<Reward> rewards = new();
+        var crewMember = new CrewMember
+        {
+            Title = "Bobby Hill",
+            Subtitle = "Gives you 10% speed boost"
+        };
+        rewards.Add(crewMember);
+        
+        var baseTransform = new GameObject().transform;
+        var buttons = new List<Button>();
+        float spacing = rewardMenu.rect.height / rewardCount + 1;
+        for(int i = 0; i < rewardCount; i++)
+        {
+            var reward = rewards[Random.Range(0, rewards.Count - 1)];
+            var transform = rewardMenu.transform;
+            transform.position = new Vector3(0, (spacing * (i + 1)));
+            var button = Instantiate(optionButton, transform);
+            buttons.Add(button);
+            var textBoxes = button.GetComponentsInChildren<TextMeshPro>();
+            foreach(var textBox in textBoxes)                                      
+            {
+                if (textBox.gameObject.name.ToLower().Contains("subtitle"))
+                {
+                    textBox.text = crewMember.Subtitle;
+                }
+                else if (textBox.gameObject.name.ToLower().Contains("title"))
+                {
+                    textBox.text = crewMember.Title;
+                }
+            }
+            button.onClick.AddListener(() =>
+            {
+                while(buttons.Count > 0)
+                {
+                    Destroy(buttons[0]);
+                }
+            });
+        }
+    }
     private void AddBounty(float bountyIncrease)
     {
         bounty += bountyIncrease;
         bountyText.text = $"${bounty * 10}00";
     }
-
     private AlienShipScript SpawnAlien(AlienShipScript shipToSpawn)
     {
         var randomNum = Random.Range(-10, 10);
@@ -109,7 +158,6 @@ public class GameManager : MonoBehaviour
         spawnDelay = 1.0f;
         return alienShip;
     }
-
     public void ShootProjectile(ProjectileType projectileType, Transform transform, Team team, float damage, float velocity, float angle)
     {
         // Adjust for 90 degree skew
@@ -141,7 +189,6 @@ public class GameManager : MonoBehaviour
             AddBounty(5);
         }
     }
-
     public class ProjectileManager
     {
         public Team owningTeam;
