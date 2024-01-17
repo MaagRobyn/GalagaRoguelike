4421392f3186f705e6ada2a2d7efbb4000e34776
MaagRobyn <maag.matthew@outlook.com>
2024-01-16 22:17:50 -0800

    Created reward modularity

---
+++ Assets/CrewMember.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets
+{
+    public class CrewMember : Reward
+    {
+        public override void GiveReward()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Reward.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets
+{
+    public abstract class Reward
+    {
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public abstract void GiveReward();
+    }
+}
