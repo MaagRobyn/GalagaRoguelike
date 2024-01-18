d61db3b347528515b7530512e1ed217b957a59e5
MaagRobyn <maag.matthew@outlook.com>
2024-01-18 13:18:16 -0800

    Created Sound Manager Created Options Menu Made Bullet trails shorter

--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -1,3 +1,4 @@
+using Assets;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
@@ -12,8 +13,21 @@ public class MenuManager : MonoBehaviour
     [SerializeField] Button optionsButton;
     [SerializeField] Button quitButton;
 
+    [SerializeField] GameObject optionMenu;
+    [SerializeField] Button closeOptionsMenuButton;
+    [SerializeField] Slider volumeSlider;
+
+    [SerializeField] Camera mainCamera;
+
     private void Start()
     {
+        if(SoundManager.Instance == null)
+        {
+            SoundManager soundManager = new();
+            soundManager.SetVolume(100);
+        }
+        volumeSlider.value = SoundManager.Instance.GetVolume();
+        
         newGameButton.onClick.AddListener(() =>
         {
             SceneManager.LoadScene(Tools.SceneConstants.GAME_SCENE);
@@ -24,11 +38,23 @@ public class MenuManager : MonoBehaviour
         });
         optionsButton.onClick.AddListener(() =>
         {
-
+            optionMenu.SetActive(true);
         });
         quitButton.onClick.AddListener(() =>
         {
             Application.Quit();
         });
+        closeOptionsMenuButton.onClick.AddListener(() =>
+        {
+            optionMenu.SetActive(false);
+        });
+        volumeSlider.onValueChanged.AddListener(x =>
+        {
+            SoundManager.Instance.SetVolume((int)x);
+        });
+    }
+    private void FixedUpdate()
+    {
+        mainCamera.transform.Rotate(new Vector3(1, 1, 0), .01f);
     }
 }
---
+++ Assets/SoundManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Assets
+{
+    public class SoundManager
+    {
+        public static SoundManager Instance { get; set; }
+        private float MasterVolume = 1;
+        private float SoundVolume = 1;
+        private float MusicVolume = 1;
+        public SoundManager() 
+        {
+            Instance = this;
+        }
+
+        public void SetVolume(int volume, SoundType type = SoundType.Master) 
+        {
+            switch (type)
+            {
+                case SoundType.Master:
+                    MasterVolume = volume > 100 ? 1 : volume / 100;
+                    break;
+                case SoundType.Music:
+                    MusicVolume = volume > 100 ? 1 : volume / 100;
+                    break;
+                case SoundType.Sound:
+                    SoundVolume = volume > 100 ? 1 : volume / 100;
+                    break;
+            }
+        }
+
+        public int GetVolume(SoundType type = SoundType.Master)
+        {
+            var output = GetVolumeFloat(type) * 100;
+            return (int)output;
+        }
+        public float GetVolumeFloat(SoundType type = SoundType.Master)
+        {
+
+            var output = type switch
+            {
+                SoundType.Master => MasterVolume,
+                SoundType.Music => MusicVolume,
+                SoundType.Sound => SoundVolume,
+                _ => 0
+            };
+            return output;
+        }
+
+        public void PlaySound(AudioSource sound, SoundType type = SoundType.Sound, float volumeModifier = 1.0f)
+        {
+            var volumeTypeMod = type switch
+            {
+                SoundType.Sound => SoundVolume,
+                SoundType.Music => MusicVolume,
+                _ => 1.0f
+            };
+            // If the volume would be more than 1.0f, set it to 1.0f
+            sound.volume = 
+                volumeModifier * MasterVolume * volumeTypeMod <= 1.0f 
+                ? volumeModifier * MasterVolume * volumeTypeMod 
+                : 1.0f;
+            sound.Play();
+        }
+
+
+        public enum SoundType
+        {
+            Master = 0,
+            Music = 1,
+            Sound = 2
+        }
+    }
+}
